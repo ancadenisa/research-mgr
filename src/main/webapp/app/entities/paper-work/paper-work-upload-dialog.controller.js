8fe21f6b96b362463f279721d04dc5380ba15404
Anca Barbu <Anca Barbu@DESKTOP-VA1FKQR>
2017-03-24 00:39:35 +0200

    upload part 2 : todo (Server side)

--- src/main/webapp/app/entities/paper-work/paper-work-upload-dialog.controller.js
+++ src/main/webapp/app/entities/paper-work/paper-work-upload-dialog.controller.js
@@ -5,12 +5,34 @@
         .module('researchMgrApp')
         .controller('PaperWorkUploadController', PaperWorkUploadController);
 
-    PaperWorkUploadController.$inject = ['$uibModalInstance', 'entity', 'PaperWork'];
+    PaperWorkUploadController.$inject = ['$uibModalInstance', 'entity', 'PaperWork', 'Upload', '$timeout'];
 
-    function PaperWorkUploadController($uibModalInstance, entity, PaperWork) {
+    function PaperWorkUploadController($uibModalInstance, entity, PaperWork, Upload, $timeout) {
         var vm = this;
 
         vm.paperWork = entity;
-
+        
+        vm.uploadFiles = function (files) {
+            vm.files = files;
+            if (files && files.length) {
+                Upload.upload({
+                    url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                    data: {
+                        files: files
+                    }
+                }).then(function (response) {
+                    $timeout(function () {
+                        vm.result = response.data;
+                    });
+                }, function (response) {
+                    if (response.status > 0) {
+                       vm.errorMsg = response.status + ': ' + response.data;
+                    }
+                }, function (evt) {
+                   vm.progress = 
+                        Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+                });
+            }
+        };
     }
 })();
