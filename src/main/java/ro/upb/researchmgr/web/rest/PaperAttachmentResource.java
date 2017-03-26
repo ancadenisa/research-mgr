154bf95541bf77e18435a636358552832cf78d6d
Anca Denisa <Anca Denisa>
2017-03-26 23:00:44 +0300

    download fix

--- src/main/java/ro/upb/researchmgr/web/rest/PaperAttachmentResource.java
+++ src/main/java/ro/upb/researchmgr/web/rest/PaperAttachmentResource.java
@@ -25,18 +25,24 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
@@ -80,21 +86,45 @@ public class PaperAttachmentResource {
     @ResponseBody
     @RequestMapping(value = "/paperAttachments/download/{id}", method = RequestMethod.GET, produces = MediaType.ALL_VALUE)
     @Timed
-    public byte[] downloadPaperWorkAttachments(@PathVariable Long id) throws URISyntaxException {
+    public void downloadPaperWorkAttachments(@PathVariable Long id, HttpServletResponse response) throws URISyntaxException, IOException {
         log.debug("REST request to download a paper attachment to paper work with id: {}", id);
         PaperAttachment paperAttachment = paperAttachmentService.findOne(id);
-    	InputStream is;
-
-    	try {
-    	    is = new FileInputStream(servletContextImpl.getRealPath(paperAttachment.getPath()));
-    	    //response.setContentType("image/jpeg");
-            //is.close();
-    	    byte[] response = IOUtils.toByteArray(is);
-    	    is.close();
-    	    return response;
-    	} catch (IOException e) {
-    		throw new RuntimeException(e);
-    	}
+        File file = new File(servletContextImpl.getRealPath(paperAttachment.getPath()));
+        
+         
+        if(!file.exists()){
+            String errorMessage = "Sorry. The file you are looking for does not exist";
+            System.out.println(errorMessage);
+            OutputStream outputStream = response.getOutputStream();
+            outputStream.write(errorMessage.getBytes(Charset.forName("UTF-8")));
+            outputStream.close();
+            return;
+        }
+         
+        String mimeType= URLConnection.guessContentTypeFromName(file.getName());
+        if(mimeType==null){
+            System.out.println("mimetype is not detectable, will take default");
+            mimeType = "application/octet-stream";
+        }
+         
+        System.out.println("mimetype : "+mimeType);
+         
+        response.setContentType(mimeType);
+         
+        /* "Content-Disposition : inline" will show viewable types [like images/text/pdf/anything viewable by browser] right on browser 
+            while others(zip e.g) will be directly downloaded [may provide save as popup, based on your browser setting.]*/
+        response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() +"\""));
+ 
+         
+        /* "Content-Disposition : attachment" will be directly download, may provide save as popup, based on your browser setting*/
+        //response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+         
+        response.setContentLength((int)file.length());
+ 
+        InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+ 
+        //Copy bytes from source to destination(outputstream in this example), closes both streams.
+        FileCopyUtils.copy(inputStream, response.getOutputStream());
     }
 
 }
--- src/main/webapp/app/entities/paper-work/paper-work-manage-uploads.controller.js
+++ src/main/webapp/app/entities/paper-work/paper-work-manage-uploads.controller.js
@@ -6,6 +6,36 @@
         .controller('PaperWorkManageUploadsController', PaperWorkManageUploadsController);
 
     PaperWorkManageUploadsController.$inject = ['$uibModalInstance', 'entity', 'PaperWork', 'Upload', '$timeout', '$http', '$window', 'FileSaver', 'Blob'];
+    
+    function getFileNameFromHttpResponse(httpResponse) {
+        var contentDispositionHeader = httpResponse.headers('Content-Disposition');
+        var result = contentDispositionHeader.split(';')[1].trim().split('=')[1];
+        return result.replace(/"/g, '');
+    }
+    
+    function b64toBlob(b64Data, contentType, sliceSize) {
+    	  contentType = contentType || '';
+    	  sliceSize = sliceSize || 512;
+
+    	  var byteCharacters = decodeURIComponent(escape(window.atob(b64Data)));
+    	  var byteArrays = [];
+
+    	  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    	    var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    	    var byteNumbers = new Array(slice.length);
+    	    for (var i = 0; i < slice.length; i++) {
+    	      byteNumbers[i] = slice.charCodeAt(i);
+    	    }
+
+    	    var byteArray = new Uint8Array(byteNumbers);
+
+    	    byteArrays.push(byteArray);
+    	  }
+    	    
+    	  var blob = new Blob(byteArrays, {type: contentType});
+    	  return blob;
+    	}
 
     function PaperWorkManageUploadsController($uibModalInstance, entity, PaperWork, Upload, $timeout, $http, $window, FileSaver, Blob) {
         var vm = this;
@@ -19,12 +49,13 @@
 		vm.downloadPaperAttachment = function(id) {
 			$http({
 				method : 'GET',
-				url : 'api/paperAttachments/download/' + id
+				url : 'api/paperAttachments/download/' + id,
+				responseType: "blob"
 			}).then(function successCallback(response) {
 				console.log(response);
-				 var blob = new Blob([response.data]);
-				 FileSaver.saveAs(blob, 'text.pdf');
-			}, function errorCallback(response) {
+				 var blob = new Blob([response.data], [{type: response.headers('content-type')}]);
+				 FileSaver.saveAs(blob, getFileNameFromHttpResponse(response));
+			}, function errorCallback(response) {	
 			});
 		}
 		
